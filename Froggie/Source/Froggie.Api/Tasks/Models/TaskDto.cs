1de3be796bbe23adec01dfac3e8c58062d826b3c
Steve Yeager <steve.f.yeager@gmail.com>
2022-08-22 12:36:30 -0700

    Use StringRangeAttribute

--- Froggie/Source/Froggie.Api/Tasks/Models/TaskDto.cs
+++ Froggie/Source/Froggie.Api/Tasks/Models/TaskDto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Froggie.Domain.Tasks.Validators;
+using LittleByte.Extensions.AspNet.Attributes;
 using LittleByte.Extensions.AspNet.Core;
 
 namespace Froggie.Api.Tasks.Models;
@@ -7,6 +8,6 @@ namespace Froggie.Api.Tasks.Models;
 public sealed class TaskDto : Dto
 {
     [Required]
-    [StringLength(TitleRules.LengthMax, MinimumLength = TitleRules.LengthMin)]
+    [StringRange(TitleRules.LengthMin, TitleRules.LengthMax)]
     public string Title { get; set; } = null!;
 }

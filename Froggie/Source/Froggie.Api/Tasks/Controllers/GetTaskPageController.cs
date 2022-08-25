c97369c51dac156e72137641b17519e2d4337468
Steve <steve.f.yeager@gmail.com>
2022-08-25 13:09:55 -0700

    Add /tasks/get-page endpoint

---
+++ Froggie/Source/Froggie.Api/Tasks/Controllers/GetTaskPageController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using Froggie.Data.Tasks.Queries;
+using LittleByte.Extensions.AspNet.Responses;
+using LittleByte.Infra.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Froggie.Api.Tasks.Controllers;
+
+[AllowAnonymous]
+public sealed class GetTaskPageController : TaskController
+{
+    private readonly ITaskPageQuery taskPageQuery;
+
+    public GetTaskPageController(ITaskPageQuery taskPageQuery)
+    {
+        this.taskPageQuery = taskPageQuery;
+    }
+
+    [HttpGet("get-page")]
+    [ResponseType(HttpStatusCode.OK, typeof(PageResponse<Task>))]
+    public async Task<ApiResponse<PageResponse<Task>>> GetPage([FromQuery]PageRequest? request)
+    {
+        request ??= new PageRequest();
+        var response = await taskPageQuery.RunQuery(request);
+        return new OkResponse<PageResponse<Task>>(response);
+    }
+}
---
+++ Froggie/Source/Froggie.Data/Tasks/Queries/TaskPageQuery.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Froggie.Data.Tasks.Queries;
+
+public interface ITaskPageQuery
+{
+    Task<PageResponse<Task>> RunQuery(PageRequest request);
+}
+
+internal sealed class TaskPageQuery : ITaskPageQuery
+{
+    private readonly FroggieDb froggieDb;
+    private readonly IMapper mapper;
+
+    public TaskPageQuery(FroggieDb froggieDb, IMapper mapper)
+    {
+        this.froggieDb = froggieDb;
+        this.mapper = mapper;
+    }
+
+    public async Task<PageResponse<Task>> RunQuery(PageRequest request)
+    {
+        var taskDaos = await froggieDb.Tasks
+            .Skip(request.Page * request.PageSize)
+            .Take(request.PageSize)
+            .ToArrayAsync();
+
+        var tasks = taskDaos
+            .Select(t => mapper.Map<Task>(t))
+            .ToArray();
+
+        var response = new PageResponse<Task>(request.PageSize, request.Page, 0, 0, tasks);
+        return response;
+    }
+}

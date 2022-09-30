5b7bb31088f72b553d13be53bc2380926a3cb116
Steve <steve.f.yeager@gmail.com>
2022-09-30 10:30:43 -0700

    Fix /tasks/get-page returning domain instead of dto

--- Froggie/Source/Froggie.Api/Tasks/Controllers/GetTaskPageController.cs
+++ Froggie/Source/Froggie.Api/Tasks/Controllers/GetTaskPageController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using AutoMapper;
+using Froggie.Api.Tasks.Models;
 using Froggie.Data.Tasks.Queries;
 using LittleByte.Common.AspNet.Core;
 using LittleByte.Common.AspNet.Responses;
@@ -10,18 +12,21 @@ namespace Froggie.Api.Tasks.Controllers;
 public sealed class GetTaskPageController : TaskController
 {
     private readonly ITaskPageQuery taskPageQuery;
+    private readonly IMapper mapper;
 
-    public GetTaskPageController(ITaskPageQuery taskPageQuery)
+    public GetTaskPageController(ITaskPageQuery taskPageQuery, IMapper mapper)
     {
         this.taskPageQuery = taskPageQuery;
+        this.mapper = mapper;
     }
 
     [HttpGet(Routes.GetByPage)]
-    [ResponseType(HttpStatusCode.OK, typeof(PageResponse<Task>))]
-    public async Task<ApiResponse<PageResponse<Task>>> GetPage([FromQuery]PageRequest? request)
+    [ResponseType(HttpStatusCode.OK, typeof(PageResponse<TaskDto>))]
+    public async Task<ApiResponse<PageResponse<TaskDto>>> GetPage([FromQuery]PageRequest? request)
     {
         request ??= new PageRequest();
         var response = await taskPageQuery.RunQuery(request);
-        return new OkResponse<PageResponse<Task>>(response);
+        var dtos = response.CastResults(mapper.Map<TaskDto>);
+        return new OkResponse<PageResponse<TaskDto>>(dtos);
     }
 }

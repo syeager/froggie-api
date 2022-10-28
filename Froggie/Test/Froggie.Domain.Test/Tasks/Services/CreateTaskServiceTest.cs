147e45fb336cdbd3ec6ff4c97c293aef75fe4c93
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-28 15:39:09 -0700

    Add tests for CreateTaskService

---
+++ Froggie/Test/Froggie.Domain.Test/Tasks/Services/CreateTaskServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Froggie.Domain.Tasks.Commands;
+using Froggie.Domain.Tasks.Services;
+
+namespace Froggie.Domain.Test.Tasks.Services;
+
+public sealed class CreateTaskServiceTest : UnitTest
+{
+    private IAddTaskCommand addTaskCommand = null!;
+    private CreateTaskService testObj = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        addTaskCommand = Substitute.For<IAddTaskCommand>();
+        testObj = new CreateTaskService(addTaskCommand);
+    }
+
+    [Test]
+    public async ValueTask With_ValidData_Then_CreateNewTask()
+    {
+        var task = await testObj.CreateAsync(Valid.Task.Title);
+
+        Assert.AreNotEqual(Guid.Empty, task.Id.Value);
+        addTaskCommand.Received(1).Add(task);
+    }
+
+    [Test]
+    public void With_InvalidData_Then_Throw()
+    {
+        Assert.ThrowsAsync<ValidationException>(() => testObj.CreateAsync("").AsTask());
+    }
+}

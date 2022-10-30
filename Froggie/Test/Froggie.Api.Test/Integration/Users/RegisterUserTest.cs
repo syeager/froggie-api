8a636df0265847f0a50f26b5edc96e6cb27b0eab
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-29 19:30:08 -0700

    Move most setup to IntegrationTest

--- Froggie/Test/Froggie.Api.Test/Integration/Users/RegisterUserTest.cs
+++ Froggie/Test/Froggie.Api.Test/Integration/Users/RegisterUserTest.cs
@@ -1,71 +1,21 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Net;
-using System.Security.Claims;
+﻿using System.Net;
 using Froggie.Api.Users;
 using Froggie.Data;
 using Froggie.Domain;
-using LittleByte.Common.Identity.Services;
-using LittleByte.Common.Logging;
-using LittleByte.Common.Logging.Configuration;
 using LittleByte.Test.Categories;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Serilog;
 
 namespace Froggie.Api.Test.Integration.Users;
 
-public class NullIdentity : ITokenGenerator
-{
-    public JwtSecurityToken GenerateJwt(IEnumerable<Claim> claims)
-    {
-        return new JwtSecurityToken();
-    }
-}
-
-public class NullDisposable : IDisposable
-{
-    public void Dispose() { }
-}
-
-public class NullLogger<T> : ILogger<T>
-{
-    public IDisposable BeginScope<TState>(TState state)
-    {
-        return new NullDisposable();
-    }
-
-    public bool IsEnabled(LogLevel logLevel)
-    {
-        return true;
-    }
-
-    public void Log<TState>(LogLevel logLevel,
-                            EventId eventId,
-                            TState state,
-                            Exception? exception,
-                            Func<TState, Exception?, string> formatter) { }
-}
-
 public sealed class RegisterUserTest : IntegrationTest
 {
-    private ServiceProvider services = null!;
-
-    [SetUp]
-    public void SetUp()
+    protected override void AddServices(IServiceCollection serviceCollection)
     {
-        var serviceCollection = new ServiceCollection()
+        serviceCollection
+            .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies())
             .AddDomain()
             .AddPersistence()
-            .AddTransient(typeof(ILogger<>), typeof(NullLogger<>))
-            .AddTransient<SecurityTokenHandler, JwtSecurityTokenHandler>()
-            .AddScoped<IDiagnosticContext, NullDiagnosticContext>()
-            .AddLogs()
-            .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies())
-            .AddTransient<ITokenGenerator, NullIdentity>()
             .AddTransient<CreateUserController>();
-
-        services = serviceCollection.BuildServiceProvider();
     }
 
     [Test]

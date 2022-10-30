4d2f7a60161469ceb29d7c9a69dcf08f1371d118
Steve Yeager <steve.f.yeager@gmail.com>
2022-10-30 12:03:59 -0700

    Remove unneeded mapper from DeleteTaskCommand

--- Froggie/Source/Froggie.Data/Tasks/Commands/DeleteTaskCommand.cs
+++ Froggie/Source/Froggie.Data/Tasks/Commands/DeleteTaskCommand.cs
@@ -1,20 +1,16 @@
-﻿using AutoMapper;
-using Froggie.Data.Tasks.Models;
-using Froggie.Domain.Tasks;
+﻿using Froggie.Domain.Tasks;
 using LittleByte.Common.Domain;
 using LittleByte.Common.Exceptions;
 
-namespace Froggie.Data.Tasks.Commands;
+namespace Froggie.Data.Tasks;
 
 internal sealed class DeleteTaskCommand : IDeleteTaskCommand
 {
     private readonly FroggieDb froggieDb;
-    private readonly IMapper mapper;
 
-    public DeleteTaskCommand(FroggieDb froggieDb, IMapper mapper)
+    public DeleteTaskCommand(FroggieDb froggieDb)
     {
         this.froggieDb = froggieDb;
-        this.mapper = mapper;
     }
 
     public async ValueTask DeleteAsync(Id<Task> id)

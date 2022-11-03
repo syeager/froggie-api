1dd77f881b4e6273867d6e84495f93b14a0df5e0
Steve Yeager <steve.f.yeager@gmail.com>
2022-11-03 01:26:27 -0700

    Add TaskFactory

---
+++ Froggie/Source/Froggie.Domain/Tasks/Factories/TaskFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Froggie.Domain.Tasks;
+
+public interface ITaskFactory
+{
+    Task Create(Guid idValue, string titleValue);
+}
+
+internal sealed class TaskFactory : ITaskFactory
+{
+    public Task Create(Guid idValue, string titleValue)
+    {
+        var title = new Title(titleValue);
+        var task = Task.Create(idValue, title);
+
+        return task;
+    }
+}
--- Froggie/Test/Froggie.Domain.Test/Tasks/Valid.cs
+++ Froggie/Test/Froggie.Domain.Test/Tasks/Valid.cs
@@ -1,4 +1,6 @@
 ﻿using Froggie.Domain.Tasks;
+using LittleByte.Common.Domain;
+using Task = Froggie.Domain.Tasks.Task;
 
 // ReSharper disable once CheckNamespace
 namespace Froggie.Domain.Test;
@@ -7,6 +9,11 @@ public static partial class Valid
 {
     public static class Tasks
     {
-        public static readonly string Title = new('a', TitleRules.LengthMin);
+        public static readonly Title Title = new(new string('a', TitleRules.LengthMin));
+
+        public static Task New()
+        {
+            return Task.Create(new Id<Task>(), Title);
+        }
     }
 }

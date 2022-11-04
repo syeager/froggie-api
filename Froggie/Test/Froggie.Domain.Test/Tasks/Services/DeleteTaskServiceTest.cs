2291d3f97db365924301a46bf379b460e9989d44
Steve Yeager <steve.f.yeager@gmail.com>
2022-11-03 17:18:34 -0700

    Add DeleteTaskServiceTest

---
+++ Froggie/Test/Froggie.Domain.Test/Tasks/Services/DeleteTaskServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Froggie.Domain.Tasks;
+
+namespace Froggie.Domain.Test.Tasks.Services;
+
+public sealed class DeleteTaskServiceTest : UnitTest
+{
+    private IDeleteTaskCommand deleteCommand = null!;
+    private DeleteTaskService testObj = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        deleteCommand = Substitute.For<IDeleteTaskCommand>();
+        testObj = new DeleteTaskService(deleteCommand);
+    }
+
+    [Test]
+    public async ValueTask With_ValidData_Then_DeleteTask()
+    {
+        var id = Guid.NewGuid();
+
+        await testObj.DeleteAsync(id);
+
+        await deleteCommand.Received(1).DeleteAsync(id);
+    }
+}

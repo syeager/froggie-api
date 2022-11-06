aa2e0e1b8edfe51bc3d5f3f009087a79de309f1a
Steve Yeager <steve.f.yeager@gmail.com>
2022-11-06 08:56:34 -0800

    Rename to GlobalUsings

---
+++ Froggie/Test/Froggie.Data.Test/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using AutoMapper;
+global using Froggie.Domain.Test;
+global using LittleByte.Test.Categories;
+global using LittleByte.Test.EntityFramework;
+global using NSubstitute;
+global using NUnit.Framework;

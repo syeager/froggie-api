5c88d41b9aff480f235198c90c0cfa3bad8dd0d6
Steve Yeager <steve.f.yeager@gmail.com>
2022-11-06 19:53:26 -0800

    Add DataIntegrationTest

---
+++ Froggie/Test/Froggie.Data.Test/DataIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using Froggie.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Froggie.Data.Test;
+
+public abstract class DataIntegrationTest : IntegrationTest
+{
+    protected sealed override void SetupInternal(IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies())
+            .AddDomain()
+            .AddPersistence();
+    }
+
+    [TearDown]
+    public override void TearDown()
+    {
+        services.GetService<FroggieDb>()?.Database.EnsureDeleted();
+
+        base.TearDown();
+    }
+}

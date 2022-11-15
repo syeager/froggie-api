3815408b05f53aab35a1be2eeedef5b3c15c062a
Steve <steve.f.yeager@gmail.com>
2022-11-15 17:34:58 -0600

    Start creating group

--- Froggie/Source/Froggie.Domain/Groups/CreateGroupService.cs
+++ Froggie/Source/Froggie.Domain/Groups/CreateGroupService.cs
@@ -2,5 +2,18 @@
 
 internal sealed class CreateGroupService
 {
-    
+    private readonly IGroupFactory groupFactory;
+
+    public CreateGroupService(IGroupFactory groupFactory)
+    {
+        this.groupFactory = groupFactory;
+    }
+
+    public async ValueTask<Group> CreateAsync(string nameValue)
+    {
+        var id = new Id<Group>();
+        var group = groupFactory.Create(id, nameValue);
+        await ValueTask.CompletedTask;
+        return group;
+    }
 }
--- Froggie/Test/Froggie.Domain.Test/Groups/CreateGroupServiceTest.cs
+++ Froggie/Test/Froggie.Domain.Test/Groups/CreateGroupServiceTest.cs
@@ -5,10 +5,24 @@ namespace Froggie.Domain.Test.Groups;
 public sealed class CreateGroupServiceTest : UnitTest
 {
     private CreateGroupService testObj = null!;
+    private IGroupFactory groupFactory = null!;
 
     [SetUp]
     public void SetUp()
     {
-        testObj = new CreateGroupService();
+        groupFactory = Substitute.For<IGroupFactory>();
+        testObj = new CreateGroupService(groupFactory);
+    }
+
+    [Test]
+    public async ValueTask When_ValidData_Then_CreateGroup()
+    {
+        var expected = Valid.Groups.New();
+        groupFactory.Create(Arg.Any<Guid>(), expected.Name)
+            .Returns(expected);
+
+        var result = await testObj.CreateAsync(expected.Name);
+
+        Assert.AreSame(expected, result);
     }
 }

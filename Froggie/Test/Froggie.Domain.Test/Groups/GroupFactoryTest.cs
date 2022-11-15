122f795eff3f978471e84715bc1e0e25ded7953b
Steve <steve.f.yeager@gmail.com>
2022-11-15 17:19:49 -0600

    Add GroupFactory

---
+++ Froggie/Test/Froggie.Domain.Test/Groups/GroupFactoryTest.cs
@@ -0,0 +1,26 @@
+﻿using Froggie.Domain.Groups;
+using LittleByte.Test.Validation;
+
+namespace Froggie.Domain.Test.Groups;
+
+public sealed class GroupFactoryTest : UnitTest
+{
+    private GroupFactory testObj = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        testObj = new GroupFactory(Validator.WillPass<Group>());
+    }
+
+    [Test]
+    public void When_ValidData_Then_CreateGroup()
+    {
+        var expected = Valid.Groups.New();
+
+        var result = testObj.Create(expected.Id, expected.Name);
+
+        // TODO: Assert that all property values are the same.
+        Assert.AreEqual(expected.Name, result.Name);
+    }
+}

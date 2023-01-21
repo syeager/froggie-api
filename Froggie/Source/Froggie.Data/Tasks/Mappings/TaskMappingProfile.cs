1cba8b4ec6d2d524c0d2b29edf4cc4ae224c8d43
Steve <steve.f.yeager@gmail.com>
2023-01-21 13:25:18 -0800

    Rename to TaskMappingProfile

---
+++ Froggie/Source/Froggie.Data/Tasks/Mappings/TaskMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using JetBrains.Annotations;
+using LittleByte.Common.AspNet.AutoMapper;
+
+namespace Froggie.Data.Tasks;
+
+[UsedImplicitly]
+internal sealed class TaskMappingProfile : Profile
+{
+    public TaskMappingProfile()
+    {
+        this.MapBothConvertOne<TaskDao, Task, TaskConverter>();
+    }
+}

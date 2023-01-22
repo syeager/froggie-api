2062b7c70e86195cf7b2bf83e6a246fd4ccd7fc5
Steve <steve.f.yeager@gmail.com>
2023-01-21 17:53:40 -0800

    Use NameRules in GroupDto

--- Froggie/Source/Froggie.Api/Groups/GroupDto.cs
+++ Froggie/Source/Froggie.Api/Groups/GroupDto.cs
@@ -1,6 +1,12 @@
-﻿namespace Froggie.Api.Groups;
+﻿using System.ComponentModel.DataAnnotations;
+using Froggie.Domain.Groups;
+using LittleByte.Common.AspNet.Attributes;
+
+namespace Froggie.Api.Groups;
 
 public sealed class GroupDto : Dto
 {
+    [Required]
+    [StringRange(NameRules.LengthMin, NameRules.LengthMax)]
     public string Name { get; init; } = null!;
 }

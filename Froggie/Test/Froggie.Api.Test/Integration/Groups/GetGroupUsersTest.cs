43cf8618ba1344a135965ec237f7c249ee0e6522
Steve Yeager <steve.f.yeager@gmail.com>
2023-03-23 19:03:41 -0700

    Move CreateGroupAndUsers to helper class

--- Froggie/Test/Froggie.Api.Test/Integration/Groups/GetGroupUsersTest.cs
+++ Froggie/Test/Froggie.Api.Test/Integration/Groups/GetGroupUsersTest.cs
@@ -1,7 +1,4 @@
 ﻿using Froggie.Api.Groups;
-using Froggie.Domain.Groups;
-using Froggie.Domain.Test;
-using Froggie.Domain.Users;
 using LittleByte.Test.AspNet;
 
 namespace Froggie.Api.Test.Integration.Groups;
@@ -12,8 +9,8 @@ public sealed class GetGroupUsersTest : ApiIntegrationTest<GetUsersGroupControll
     public async ValueTask QueryAllUsers()
     {
         const int userCount = 3;
-        var group = await CreateGroupAndUsers("group", userCount);
-        _ = await CreateGroupAndUsers("groupother", 2);
+        var group = await CreateGroupAndUsersHelper.CreateAsync(services, "group", userCount);
+        _ = await CreateGroupAndUsersHelper.CreateAsync(services, "group-other", 2);
         await saveCommand.CommitChangesAsync();
 
         var users = await controller.GetUsers(group);
@@ -21,26 +18,4 @@ public sealed class GetGroupUsersTest : ApiIntegrationTest<GetUsersGroupControll
         ApiAssert.IsSuccess(users);
         Assert.AreEqual(userCount, users.Obj!.TotalResults);
     }
-
-    #region Helpers
-
-    private async ValueTask<Group> CreateGroupAndUsers(string groupName, int userCount)
-    {
-        var registerUser = GetService<IUserRegisterService>();
-        var createGroup = GetService<ICreateGroupService>();
-        var addUserToGroup = GetService<IAddUserToGroupService>();
-
-        var group = await createGroup.CreateAsync(groupName);
-
-        for(var i = 0; i < userCount; i++)
-        {
-            var name = $"{group.Name.Value}{i}";
-            var user = await registerUser.RegisterAsync($"{name}@mail.com", name, Valid.Users.Password);
-            await addUserToGroup.AddAsync(user, group);
-        }
-
-        return group;
-    }
-
-    #endregion
 }

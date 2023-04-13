baef1d5411e26b5785fddfdee0d92e97fdc68212
Steve Yeager <steve.f.yeager@gmail.com>
2023-04-12 21:06:24 -0700

    Add seed data for test user

--- Froggie/Source/Froggie.Api/Program.cs
+++ Froggie/Source/Froggie.Api/Program.cs
@@ -48,4 +48,6 @@ app
     .UseEndpoints(endpoints => endpoints.MapControllers())
     .UseOpenApi();
 
+app.AddSeedData(app.Services.CreateScope().ServiceProvider);
+
 app.Run();
---
+++ Froggie/Source/Froggie.Data/SeedData.cs
@@ -0,0 +1,23 @@
+﻿using Froggie.Domain.Users;
+using LittleByte.Common.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Froggie.Data;
+
+public static class SeedData
+{
+    public static IApplicationBuilder AddSeedData(this IApplicationBuilder @this, IServiceProvider services)
+    {
+        var froggieDb = services.GetRequiredService<FroggieDb>();
+
+        if(froggieDb.Users.IsEmpty())
+        {
+            var userRegisterService = services.GetRequiredService<IUserRegisterService>();
+            _ = userRegisterService.RegisterAsync("user@froggie.com", "Test User", "abc").Result;
+            froggieDb.SaveChanges();
+        }
+
+        return @this;
+    }
+}

052749e217d26eb021d7368e082f2d9abc79e407
Steve Yeager <steve.f.yeager@gmail.com>
2023-04-12 20:37:56 -0700

    Add endpoint to get a user's groups

---
+++ Froggie/Source/Froggie.Api/Groups/GetUserGroupsController.cs
@@ -0,0 +1,26 @@
+﻿using Froggie.Domain.Groups;
+using Froggie.Domain.Users;
+using LittleByte.Common.AspNet.AutoMapper;
+
+namespace Froggie.Api.Groups;
+
+public sealed class GetUserGroupsController : GroupController
+{
+    private readonly IGetUsersGroupsQuery getGroupsQuery;
+
+    public GetUserGroupsController(IGetUsersGroupsQuery getGroupsQuery, IMapper mapper)
+        : base(mapper)
+    {
+        this.getGroupsQuery = getGroupsQuery;
+    }
+
+    [HttpGet("user-groups")]
+    [ResponseType(HttpStatusCode.OK, typeof(GroupDto[]))]
+    public async ValueTask<ApiResponse<GroupDto[]>> GetGroups(Guid id)
+    {
+        var userId = new Id<User>(id);
+        var groups = await getGroupsQuery.QueryAsync(userId);
+        var dtos = mapper.MapRange<GroupDto>(groups);
+        return new OkResponse<GroupDto[]>(dtos);
+    }
+}

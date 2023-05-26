7894d811cd3f8814c6b39297fa5b167ff348c1e7
Steve <steve.f.yeager@gmail.com>
2023-05-26 16:31:43 -0500

    Add endpoint to add user to group

---
+++ Froggie/Source/Froggie.Api/Groups/AddUserToGroupController.cs
@@ -0,0 +1,40 @@
+﻿using Froggie.Domain.Groups;
+using Froggie.Domain.Users;
+using LittleByte.Common.Infra.Commands;
+
+namespace Froggie.Api.Groups;
+
+public sealed class AddUserToGroupController : GroupController
+{
+    private readonly IFindByIdQuery<User> findUserQuery;
+    private readonly IFindByIdQuery<Group> findGroupQuery;
+    private readonly IAddUserToGroupService addUserToGroupService;
+    private readonly ISaveContextCommand saveCommand;
+
+    public AddUserToGroupController(IMapper mapper,
+                                    IFindByIdQuery<User> findUserQuery,
+                                    IFindByIdQuery<Group> findGroupQuery,
+                                    IAddUserToGroupService addUserToGroupService,
+                                    ISaveContextCommand saveCommand)
+        : base(mapper)
+    {
+        this.findUserQuery = findUserQuery;
+        this.findGroupQuery = findGroupQuery;
+        this.addUserToGroupService = addUserToGroupService;
+        this.saveCommand = saveCommand;
+    }
+
+    [HttpPost("add-member")]
+    [ResponseType(HttpStatusCode.OK)]
+    public async ValueTask<ApiResponse> AddUser(AddUserToGroupRequest request)
+    {
+        var user = await findUserQuery.FindRequiredAsync(request.UserId);
+        var group = await findGroupQuery.FindRequiredAsync(request.GroupId);
+
+        await addUserToGroupService.AddAsync(user, group);
+
+        await saveCommand.CommitChangesAsync();
+
+        return new OkResponse();
+    }
+}
---
+++ Froggie/Test/Froggie.Api.Test/Integration/Groups/AddUserToGroupTest.cs
@@ -0,0 +1,23 @@
+﻿using Froggie.Api.Groups;
+using Froggie.Domain.Test;
+using Froggie.Domain.Users;
+using LittleByte.Test.AspNet;
+
+namespace Froggie.Api.Test.Integration.Groups;
+
+public sealed class AddUserToGroupTest : ApiIntegrationTest<AddUserToGroupController>
+{
+    [Test]
+    public async ValueTask AddNewUserToGroup()
+    {
+       var group = await CreateGroupAndUsersHelper.CreateAsync(services);
+       var user = Valid.Users.New();
+       await GetService<IAddUserCommand>().AddAsync(user, Valid.Users.Password);
+       await saveCommand.CommitChangesAsync();
+
+       var request = new AddUserToGroupRequest(user, group);
+       var response = await controller.AddUser(request);
+
+       ApiAssert.IsSuccess(response);
+    }
+}

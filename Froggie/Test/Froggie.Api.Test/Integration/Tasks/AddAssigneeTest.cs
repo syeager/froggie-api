5b9e35b3258702db6ea2671b0f175756e58b1ddf
Steve <steve.f.yeager@gmail.com>
2023-05-29 18:46:02 -0500

    Add AddNewAssignee test

--- Froggie/Test/Froggie.Api.Test/Integration/Tasks/AddAssigneeTest.cs
+++ Froggie/Test/Froggie.Api.Test/Integration/Tasks/AddAssigneeTest.cs
@@ -1,8 +1,21 @@
 ﻿using Froggie.Api.Tasks;
+using Froggie.Api.Test.Integration.Groups;
+using LittleByte.Test.AspNet;
 
 namespace Froggie.Api.Test.Integration.Tasks;
 
 public sealed class AddAssigneeTest : ApiIntegrationTest<AddAssigneeController>
 {
-    
+    [Test]
+    public async ValueTask AddNewAssignee()
+    {
+        var (group, users) = await CreateGroupAndUsersHelper.CreateAsync(services, userCount: 1);
+        await saveCommand.CommitChangesAsync();
+        var userId = users.First().Id;
+
+        var response = await controller.AddAssignee();
+
+        ApiAssert.IsSuccess(response);
+        CollectionAssert.Contains(response.Obj!.Assignees, userId);
+    }
 }

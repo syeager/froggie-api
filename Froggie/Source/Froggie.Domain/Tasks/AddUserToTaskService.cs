223f85ac1735e82370a91985a61cfcaaea7ffdcd
Steve <steve.f.yeager@gmail.com>
2023-06-27 17:42:35 -0700

    Start adding user to task

---
+++ Froggie/Source/Froggie.Domain/Tasks/AddUserToTaskService.cs
@@ -0,0 +1,30 @@
+﻿using Froggie.Domain.Groups;
+using Froggie.Domain.Users;
+using LittleByte.Common.Infra.Queries;
+using LittleByte.Common.Tasks;
+
+namespace Froggie.Domain.Tasks;
+
+public interface IAddUserToTaskService
+{
+    ValueTask<Task> AddAsync(Id<User> userId, Id<Task> taskId);
+}
+
+internal sealed class AddUserToTaskService : IAddUserToTaskService
+{
+    private readonly IFindByIdQuery<Task> findTaskQuery;
+
+    public AddUserToTaskService(IFindByIdQuery<Task> findTaskQuery)
+    {
+        this.findTaskQuery = findTaskQuery;
+    }
+
+    public async ValueTask<Task> AddAsync(Id<User> userId, Id<Task> taskId)
+    {
+        var task = await findTaskQuery.FindRequiredForEditAsync(taskId).NoWait();
+
+        task.AddAssignee(userId);
+
+        return task;
+    }
+}
---
+++ Froggie/Test/Froggie.Domain.Test/Tasks/AddUserToTaskTest.cs
@@ -0,0 +1,34 @@
+﻿using Froggie.Domain.Tasks;
+using LittleByte.Common.Infra.Queries;
+
+namespace Froggie.Domain.Test.Tasks;
+
+// user not in group
+// task doesn't exist
+// user doesn't exist
+public sealed class AddUserToTaskTest : UnitTest
+{
+    private AddUserToTaskService testObj = null!;
+    private IFindByIdQuery<Task> findTaskQuery = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        findTaskQuery = Substitute.For<IFindByIdQuery<Task>>();
+
+        testObj = new AddUserToTaskService(findTaskQuery);
+    }
+
+    [Test]
+    public async ValueTask AddUserToTask()
+    {
+        var user = Valid.Users.New();
+        var group = Valid.Groups.New();
+        var task = Valid.Tasks.New(user, group);
+        findTaskQuery.FindRequiredForEditAsync(task).Returns(task);
+
+        await testObj.AddAsync(user, task);
+
+        CollectionAssert.Contains(task.Assignees, user.Id);
+    }
+}

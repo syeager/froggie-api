ec77dc54e2b85ee9705f9f1512345b1249e70fbd
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-13 18:43:39 -0700

    Remove unnecessary ApiController on UserController

--- Froggie/Source/Froggie.Api/Users/UserController.cs
+++ Froggie/Source/Froggie.Api/Users/UserController.cs
@@ -5,5 +5,4 @@ namespace Froggie.Api.Users;
 [AllowAnonymous]
 [Route("users", Name = "Users")]
 [OpenApiTag("Users")]
-[ApiController]
-public abstract class UserController : Controller { }
+public abstract class UserController : Controller;

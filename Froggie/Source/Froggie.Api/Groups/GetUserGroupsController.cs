238d9c59c3acf61b6f28b92b5c03938a275182c8
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-14 13:02:17 -0700

    Use object for param in GetUserGroupsController

--- Froggie/Source/Froggie.Api/Groups/GetUserGroupsController.cs
+++ Froggie/Source/Froggie.Api/Groups/GetUserGroupsController.cs
@@ -1,24 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
 using Froggie.Domain.Groups;
 using Froggie.Domain.Users;
 using LittleByte.AutoMapper;
 
 namespace Froggie.Api.Groups;
 
-public sealed class GetUserGroupsController : GroupController
-{
+public sealed record GetUserGroupsRequest([Required] Guid UserId);
+
 public sealed class GetUserGroupsController(IGetUserGroupsQuery getGroupsQuery, IMapper mapper)
     : GroupController
 {
-        : base(mapper)
-    {
-        this.getGroupsQuery = getGroupsQuery;
-    }
-
+    // TODO: Return Page.
     [HttpGet("user-groups")]
     [ResponseType(HttpStatusCode.OK, typeof(GroupDto[]))]
-    public async ValueTask<ApiResponse<GroupDto[]>> GetGroups(Guid id)
+    public async ValueTask<ApiResponse<GroupDto[]>> GetGroups([FromQuery] GetUserGroupsRequest request)
     {
-        var userId = new Id<User>(id);
+        var userId = new Id<User>(request.UserId);
         var groups = await getGroupsQuery.QueryAsync(userId);
         var dtos = mapper.MapRange<GroupDto>(groups);
         return new OkResponse<GroupDto[]>(dtos);
